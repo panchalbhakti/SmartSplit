3dad400dc393cdd9365bff85aee4779d695c1c20
Vaidehi Nayakpura <149557062+Vaidehi1005@users.noreply.github.com>
2024-12-04 23:27:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -561,3 +561,24 @@ splitMethodSelect.addEventListener('change', () => {
   alert(`Default split method changed to: ${selectedMethod === 'standard' ? 'Standard Split' : 'Budget-Based Split'}`);
 });
 
+//Javascript for the Payment Gateway
+//Function to format card number with spaces
+ function formatCardNumber(input) {
+            let value = input.value.replace(/\D/g, ''); // Remove non-numeric characters
+            if (value.length <= 16) {
+                value = value.replace(/(\d{4})(?=\d)/g, '$1 '); // Insert spaces after every 4 digits
+            }
+            input.value = value;
+        }
+// Function to format expiry month
+ function formatExpiryMonth(input) {
+            let value = input.value.replace(/\D/g, ''); // Remove non-numeric characters
+            if (value.length > 4) {
+                value = value.slice(0, 4); // Limit input to 4 digits
+            }
+            if (value.length > 2) {
+                value = value.replace(/(\d{2})(\d{1,2})/, '$1 / $2'); // Insert " / " after the first 2 digits
+            }
+            input.value = value;
+        }
+
