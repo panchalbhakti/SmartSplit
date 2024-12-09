e57dda8aecf1150a7270ec78c4feb4e101391432
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-12-09 12:57:14 +0530

    add

--- SmartSplit/Sign-up/entry.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.toggle-password').forEach(item => {
-    item.addEventListener('click', function() {
-        const passwordField = document.querySelector(this.getAttribute('data-toggle'));
-        const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
-        passwordField.setAttribute('type', type);
-        this.classList.toggle('fa-eye-slash');
-    });
-});
